72aaeecb122a68b94b3752b20f1dc26ae443173b
Aaron <aaronpcoffey@gmail.com>
2025-06-09 23:12:58 -0500

    Contact Page UI

--- frontend/src/pages/contact.jsx
+++ frontend/src/pages/contact.jsx
@@ -1,9 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Contact = () => {
+    const [formData, setFormData] = useState(
+        {
+            name:'',
+            phone:'',
+            email:'',
+            message:'',
+        }
+    )
+
+    const handleChange = (e) => {
+        setFormData((prev) => ({
+            ...prev,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const handleSubmit = (e) => {
+        
+    }
+
     return (
-        <div>
-            Contact
+        <div className="contact page">
+            <div className="bio card">
+            {['name', 'phone', 'email', 'message'].map((field) => (
+                <div className="contact field container">
+                    <label className="contact label">
+                        {field.toUpperCase()}
+                    </label>
+                    {field == 'message' ? (
+                        <textarea
+                            name="message"
+                            value={formData.message}
+                            onChange={handleChange}
+                            style={{ width: '100%', padding: '10px', backgroundColor: 'white', minHeight: '150px', border: '1px solid #ccc', borderRadius: '4px',}}
+                        />
+                    ) : (
+                        <input
+                            type="text"
+                            name={{field}}
+                            value={formData.field}
+                            onChange={handleChange}
+                            style={{ width: '100%', padding: '10px', backgroundColor: 'white', border: '1px solid #ccc', borderRadius: '4px', }}
+                        />
+                    )}
+                </div>
+            ))}
+            </div>
+            <div style={{display:'flex', justifyContent:'center'}}>
+                <button type="submit" onClick={handleSubmit} style={{padding: '10px 20px', justifyContent: 'center'}}>Send</button>
+            </div>
         </div>
     )
 }
